4f06209f4598108132a376603ff7e15612635126
Charles Lamb Jr <charleslambjr@Charless-MacBook-Pro.local>
2023-11-07 08:06:49 -0500

    added the procfile

--- server.js
+++ server.js
@@ -120,5 +120,5 @@ app.put('/update/:id', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
