8bea1117bbda99fa1cecf9ee0823dca8df351e44
Charles Lamb Jr <charleslambjr@Charless-MacBook-Pro.local>
2023-11-07 20:07:40 -0500

    added static file to be served by the server if routes are not recongnized

--- server.js
+++ server.js
@@ -119,6 +119,17 @@ app.put('/update/:id', async (req, res) => {
     res.json(result);
 });
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
