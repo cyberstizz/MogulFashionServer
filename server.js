4ab4775b3b59241eb39780c7553bb102477e685f
Charles Lamb Jr <charleslambjr@Charless-MacBook-Pro.local>
2023-11-08 05:31:49 -0500

    added the 0.9.9 explicitly

--- server.js
+++ server.js
@@ -134,6 +134,6 @@ if (process.env.NODE_ENV === 'production') {
 const port = process.env.PORT || 4000;
 
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
 });
